b9b2c4f7c6defe0cfcb83b5b3f5031ddfabe1d60
Hiroki Noda <kubo39@gmail.com>
2017-12-26 23:15:39 +0900

    Initial commit

---
+++ helloworld/source/app.d
@@ -0,0 +1,62 @@
+import x11.X;
+import x11.Xlib;
+
+void main()
+{
+    // Open display connection.
+    auto display = XOpenDisplay(null);
+    assert(display !is null, "XOpenDisplay failed.");
+
+    // Create window.
+    auto screen = XDefaultScreen(display);
+    auto root  =XRootWindow(display, screen);
+
+    XSetWindowAttributes attributes = void;
+    attributes.background_pixel = XWhitePixel(display, screen);
+
+    auto window = XCreateWindow(display, root,
+                                0, 0, 400, 300,
+                                0, 0,
+                                InputOutput, null,
+                                CWBackPixel, &attributes);
+
+    // Set window title.
+    auto title = cast(char*) "Hello-world\0".ptr;
+    XStoreName(display, window, title);
+
+    // Hook close request.
+    auto wmProtocol = "WM_PROTOCOL\0".ptr;
+    auto wmDeleteWindowStr = "WM_DELETE_WINDOW\0".ptr;
+
+    auto wmProtocols = XInternAtom(display, wmProtocol, False);
+    auto wmDeleteWindow = XInternAtom(display, wmDeleteWindowStr, False);
+
+    // Show window.
+    XMapWindow(display, window);
+
+    // Mainloop.
+    XEvent event = void;
+
+    while (true)
+    {
+        XNextEvent(display, &event);
+
+        switch (event.type)
+        {
+        case ClientMessage:
+            auto xclient = event.xclient;
+
+            if (xclient.message_type == wmProtocols && xclient.format == 32)
+            {
+                auto protocol = cast(Atom) xclient.data.l[0];
+
+                if (protocol == wmDeleteWindow)
+                    goto END;
+            }
+            break;
+        default:
+            // nop
+        }
+    }
+END:
+}
